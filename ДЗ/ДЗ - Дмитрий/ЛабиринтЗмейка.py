55f8a9315854cc676be9adbe5105de2e05c7e8e7
Dimon <peyton.b1329@yandex.ru>
2018-10-27 18:37:40 +0300

    тут питон

---
+++ ДЗ/ДЗ - Дмитрий/ЛабиринтЗмейка.py
@@ -0,0 +1,59 @@
+way = list()
+for i in range(10):
+    way.append(1)
+print(way)
+way.clear()
+way.append(1)
+for i in range(8):
+    way.append(0)
+way.append(1)
+print(way)
+way.clear()
+for i in range(8):
+    way.append(1)
+way.append(0)
+way.append(1)
+print(way)
+way.clear()
+way.append(1)
+for i in range(8):
+    way.append(0)
+way.append(1)
+print(way)
+way.clear()
+way.append(1)
+way.append(0)
+for i in range(8):
+    way.append(1)
+print(way)
+way.clear()
+way.append(1)
+for i in range(8):
+    way.append(0)
+way.append(1)
+print(way)
+way.clear()
+for i in range(8):
+    way.append(1)
+way.append(0)
+way.append(1)
+print(way)
+way.clear()
+way.append(1)
+for i in range(8):
+    way.append(0)
+way.append(1)
+print(way)
+way.clear()
+way.append(1)
+way.append(0)
+for i in range(8):
+    way.append(1)
+print(way)
+way.clear()
+for i in range(10):
+    way.append(1)
+print(way)
+way.clear()
+
+
