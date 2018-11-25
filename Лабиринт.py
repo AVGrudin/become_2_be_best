71244ef16e264976ada7bb5e97ff1a0aada58fbc
AVGrudin <AVGrudin@gmail.com>
2018-11-25 13:08:32 +0300

    [ADD] Заготовка для лабиринта

---
+++ Лабиринт.py
@@ -0,0 +1,45 @@
+
+
+class Sprite:
+    def __init__(self, x, y):
+        pass
+
+    def move_up(self):
+        pass
+
+    def move_down(self):
+        pass
+
+    def move_left(self):
+        pass
+
+    def move_right(self):
+        pass
+
+    def event_listener(event)
+
+
+class Maze:
+    def __init__(self, maze_map):
+        pass
+
+    def update(self):
+        pass
+
+    def move_up(self):
+        pass
+
+    def move_down(self):
+        pass
+
+    def move_left(self):
+        pass
+
+    def move_right(self):
+        pass
+
+
+
+
+while True:
+    pass
