d61a8c1d5ccfd7fc6a6a7f32908af4498795bb4a
Dimon <peyton.b1329@yandex.ru>
2019-01-19 16:47:21 +0300

    всё

--- Лабиринт.py
+++ Лабиринт.py
@@ -40,6 +40,16 @@ class Maze:
 
 
 
+maze_map = [[1, 1, 1, 1, 1],[1,'E',0,0,1],[1, 1, 1, 0, 1],[1, 'S', 0, 0, 1],[1, 1, 1, 1, 1]]
+
+HEIGHT = 1000
+WIDTH = 1000
+window = Tk()
+window.title('Bubbles')
+c = Canvas(window, width=WIDTH, height=HEIGHT, bg='blue')
+c.pack()
+maze = Maze(window, maze_map, c)
+
 
 while True:
     pass
