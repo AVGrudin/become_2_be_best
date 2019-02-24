1c7904984b26fc6b3446571cc062071d9e951dc9
slavas555 <skurixin06@mail.ru>
2019-02-24 11:38:10 +0300

    добавлена война самолетов в ДЗ

---
+++ ДЗ/ДЗ - Вячеслав/война самолетов.py
@@ -0,0 +1,36 @@
+from tkinter import *
+
+
+
+player_x = 0
+player_y = 0
+player_tkinter_id = 0
+player_col = "red"
+player_lives = 3
+
+
+enemies_x = []
+enemies_y = []
+enemies_rect_id = []
+enemy_col = "perpel"
+enemies_num_in_group = 10
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
