88a217d8b681fe428e1bf016c4213a7129587967
Dimon <peyton.b1329@yandex.ru>
2019-03-10 11:12:00 +0300

    вац

--- ДЗ/ДЗ - Дмитрий/2квадрата.py
+++ ДЗ/ДЗ - Дмитрий/2квадрата.py
@@ -6,7 +6,7 @@ window = Tk()
 window.title('Квадраты')
 c = Canvas(window, width=WIDTH, height=HEIGHT)
 c.pack()
-c.create_rectangle(0, 0, 400, 400, fill = 'blue')
+c.create_rectangle(0, 0, 400, 399, fill = 'blue')
 c.create_rectangle(400, 0, 800, 400, fill = 'red')
 
 while True:
